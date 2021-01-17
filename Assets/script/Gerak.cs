a066375c4fbdc98f844ab4b9890189a7425f5a55
mubarakabaz <abdulazizmubarak1@gmail.com>
2021-01-17 23:56:36 +0800

    add library and assets

---
+++ Assets/script/Gerak.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gerak : MonoBehaviour {
+
+	public float speed = 5f;
+	public float jumpspeed = 8f;
+	private Rigidbody2D rigidBody;
+	private float movement = 0f;
+	public Transform groundCheckPoint;
+	public float groundCheckRadius;
+	public LayerMask groundLayer;
+	public bool isTouchingGround;
+	// Use this for initialization
+	void Start () {
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		isTouchingGround = Physics2D.OverlapCircle(groundCheckPoint.position,groundCheckRadius,groundLayer);
+		movement = Input.GetAxis("Horizontal");
+
+		if(Input.GetKey(KeyCode.D)){
+			rigidBody.velocity = new Vector2(movement*speed,rigidBody.velocity.y);
+		}
+		if(Input.GetKey(KeyCode.A)){
+			rigidBody.velocity = new Vector2(movement*speed, rigidBody.velocity.y);
+		}
+		if (Input.GetKey(KeyCode.W) && isTouchingGround){
+			rigidBody.velocity = new Vector2(rigidBody.velocity.x,jumpspeed);
+		}
+		{
+			
+		}
+	}
+}
